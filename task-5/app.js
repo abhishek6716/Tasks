c895b6cad23d8d20ad97c6a2730aca5d5dc2f266
Abhishek singh <abhisheksingh6716@gmail.com>
2021-05-18 18:53:44 +0530

    edited code

--- task-5/app.js
+++ task-5/app.js
@@ -38,115 +38,115 @@ getExperience('RELEVANT', (error, data) => {
     }
 })
 
-const getExperience = (experienceType, callback) => {
-    const request = new XMLHttpRequest()
+// const getExperience = (experienceType, callback) => {
+//     const request = new XMLHttpRequest()
 
-    request.addEventListener('readystatechange', (e) => {
-        if (e.target.readyState === 4 && e.target.status === 200) {
-            const data = JSON.parse(e.target.responseText)
-            callback(undefined, data)
-        } else if (e.target.readyState === 4) {
-            callback('An error has taken place', undefined)
-        }
-    })
-    request.open('GET', `https://api-hfc.techchefz.com/icicihfc-micro-service/rms/get/experience?experienceType=${experienceType}`)
-    request.send()
-}
-
-
-
-
-// OverallExperience and RelevantExperience
-
-let overallExpData, relevantExpData;                                 
-const getOverallExpData = async () => {
-    const body = await getExperience('OVERALL')
-    overallExpData = body.data
-    console.log(body)
-    setOverallEl()
-}
-const getRelevantExpData = async () => {
-    const body = await getExperience('RELEVANT')
-    relevantExpData = body.data
-    setReleventEl()
-}
-
-function setOverallEl(){
-    for(let i=0; i<overallExpData.length; i++){
-        const OptEl = document.createElement('option')
-        OptEl.textContent = overallExpData[i].value
-        OptEl.setAttribute('id', 'overallExpData[i].id')
-        OptEl.setAttribute('onclick', 'getID(this)')
-        overallExperience.append(OptEl)
-    }
-}
-
-function setReleventEl() {
-    for (let i = 0; i < relevantExpData.length; i++) {
-        const OptEl = document.createElement('option')
-        OptEl.textContent = relevantExpData[i].value
-        OptEl.setAttribute('id', 'relevantExpData[i].id')
-        OptEl.setAttribute('onclick', 'getID(this)')
-        relaventExperience.append(OptEl)
-    }
-}
+//     request.addEventListener('readystatechange', (e) => {
+//         if (e.target.readyState === 4 && e.target.status === 200) {
+//             const data = JSON.parse(e.target.responseText)
+//             callback(undefined, data)
+//         } else if (e.target.readyState === 4) {
+//             callback('An error has taken place', undefined)
+//         }
+//     })
+//     request.open('GET', `https://api-hfc.techchefz.com/icicihfc-micro-service/rms/get/experience?experienceType=${experienceType}`)
+//     request.send()
+// }
 
 
 
-// Departments and Roles
 
-let departmentAndRoleData
-const getDepAndRoleData = async () => {
-    const body = await getDepAndRole()
-    departmentAndRoleData = body.data
-    setDepEl()
-}
+// // OverallExperience and RelevantExperience
 
-function setDepEl() {
-    role.disabled = true
-    for (let i = 0; i < departmentAndRoleData.length; i++) {
-        const OptEl = document.createElement('option')
-        OptEl.textContent = departmentAndRoleData[i].name
-        OptEl.setAttribute('id', 'departmentAndRoleData[i].id')
-        OptEl.setAttribute('onclick', 'getID(this)')
-        department.append(OptEl)
-    }
-    setRoleEl(target.id)
-}
+// let overallExpData, relevantExpData;                                 
+// const getOverallExpData = async () => {
+//     const body = await getExperience('OVERALL')
+//     overallExpData = body.data
+//     console.log(body)
+//     setOverallEl()
+// }
+// const getRelevantExpData = async () => {
+//     const body = await getExperience('RELEVANT')
+//     relevantExpData = body.data
+//     setReleventEl()
+// }
 
-function setRoleEl(selectedDep) {
-    for (let i = 0; i < departmentAndRoleData.length; i++) {
-        if (departmentAndRoleData[i].id == selectedDep) {
-            for (let j=0; j<departmentAndRoleData[i][j]; j++) {
-                const OptEl = document.createElement('option')
-                OptEl.textContent = departmentAndRoleData[i][j].name
-                OptEl.setAttribute('id', 'departmentAndRoleData[i][j].id')
-                OptEl.setAttribute('onclick', 'getID(this)')
-                role.append(OptEl)
-            }
-        }
-    }
-    role.disabled = false
-}
+// function setOverallEl(){
+//     for(let i=0; i<overallExpData.length; i++){
+//         const OptEl = document.createElement('option')
+//         OptEl.textContent = overallExpData[i].value
+//         OptEl.setAttribute('id', 'overallExpData[i].id')
+//         OptEl.setAttribute('onclick', 'getID(this)')
+//         overallExperience.append(OptEl)
+//     }
+// }
+
+// function setReleventEl() {
+//     for (let i = 0; i < relevantExpData.length; i++) {
+//         const OptEl = document.createElement('option')
+//         OptEl.textContent = relevantExpData[i].value
+//         OptEl.setAttribute('id', 'relevantExpData[i].id')
+//         OptEl.setAttribute('onclick', 'getID(this)')
+//         relaventExperience.append(OptEl)
+//     }
+// }
 
 
-// zones and branches
 
-let zoneData
-const getZonesAndBranch = async () => {
-    const body = await getZones()
-    zoneData = body.data
-    setZonesEl()
-}
+// // Departments and Roles
 
-function setZonesEl(){
-    branch.disabled = true
-    for(let i=0; i<zoneData.length; i++){
-        const OptEl = document.createElement('option')
-        OptEl.textContent = zoneData[i]
-        zone.append(OptEl)
-    }
-}
+// let departmentAndRoleData
+// const getDepAndRoleData = async () => {
+//     const body = await getDepAndRole()
+//     departmentAndRoleData = body.data
+//     setDepEl()
+// }
+
+// function setDepEl() {
+//     role.disabled = true
+//     for (let i = 0; i < departmentAndRoleData.length; i++) {
+//         const OptEl = document.createElement('option')
+//         OptEl.textContent = departmentAndRoleData[i].name
+//         OptEl.setAttribute('id', 'departmentAndRoleData[i].id')
+//         OptEl.setAttribute('onclick', 'getID(this)')
+//         department.append(OptEl)
+//     }
+//     setRoleEl(target.id)
+// }
+
+// function setRoleEl(selectedDep) {
+//     for (let i = 0; i < departmentAndRoleData.length; i++) {
+//         if (departmentAndRoleData[i].id == selectedDep) {
+//             for (let j=0; j<departmentAndRoleData[i][j]; j++) {
+//                 const OptEl = document.createElement('option')
+//                 OptEl.textContent = departmentAndRoleData[i][j].name
+//                 OptEl.setAttribute('id', 'departmentAndRoleData[i][j].id')
+//                 OptEl.setAttribute('onclick', 'getID(this)')
+//                 role.append(OptEl)
+//             }
+//         }
+//     }
+//     role.disabled = false
+// }
+
+
+// // zones and branches
+
+// let zoneData
+// const getZonesAndBranch = async () => {
+//     const body = await getZones()
+//     zoneData = body.data
+//     setZonesEl()
+// }
+
+// function setZonesEl(){
+//     branch.disabled = true
+//     for(let i=0; i<zoneData.length; i++){
+//         const OptEl = document.createElement('option')
+//         OptEl.textContent = zoneData[i]
+//         zone.append(OptEl)
+//     }
+// }
 
 
 
