d5f3c624d8028b4c47074942ca50ba58e803fca5
Abhishek singh <abhisheksingh6716@gmail.com>
2021-05-20 09:28:50 +0530

    removed bugs

--- task-5/app.js
+++ task-5/app.js
@@ -1,57 +1,59 @@
-console.log('app is running!')
+console.log('app is running!');
 
-const nodeFullName = document.getElementById('fullName')                          // input box
-const nodeEmail = document.getElementById('email')                                // input box
-const nodeMobileNo = document.getElementById('mobileNo')                          // input box
-const overallExperience = document.getElementById('overallExperience')            // toggler
-const relaventExperience = document.getElementById('relaventExperience')          // toggler
-const department = document.getElementById('department')                          // toggler
-const role = document.getElementById('role')                                      // toggler
-const zone = document.getElementById('zone')                                      // toggler
-const branch = document.getElementById('branch')                                  // toggler
-const attachFile = document.getElementById('attachFile')                          // doc
-const button = document.getElementById('button')                                  // button
+const nodeFullName = document.getElementById('fullName'); // input box
+const nodeEmail = document.getElementById('email'); // input box
+const nodeMobileNo = document.getElementById('mobileNo'); // input box
+const overallExperience = document.getElementById('overallExperience'); // toggler
+const relaventExperience = document.getElementById('relaventExperience'); // toggler
+const department = document.getElementById('department'); // toggler
+const role = document.getElementById('role'); // toggler
+const zone = document.getElementById('zone'); // toggler
+const branch = document.getElementById('branch'); // toggler
+const attachFile = document.getElementById('attachFile'); // doc
+const button = document.getElementById('button'); // button
 
-let fullName, emailID, mobileNo, experienceOverallID, experienceRelavantID, roleID, jobLocationID, resumeID, resumeName, resumeNonce;
+let fullName,
+    emailID,
+    mobileNo,
+    experienceOverallID,
+    experienceRelavantID,
+    roleID,
+    jobLocationID,
+    resumeID,
+    resumeName,
+    resumeNonce;
 
 /////////////////////// fullname email mobileno ////////////////////////
 nodeFullName.addEventListener('change', () => {
-    let regex = /^[a-z]([-']?[a-z]+)*( [a-z]([-']?[a-z]+)*)+$/;
-    let str = nodeFullName.value
+    const regex = /^[a-z]([-']?[a-z]+)( [a-z]([-']?[a-z]+))+$/;
+    const str = nodeFullName.value;
     if (regex.test(str)) {
-        fullName = str
-        // validFirstName = true
+        fullName = str;
     } else {
-        alert('enter valid full name!')
-        // validFirstName = false
+        alert('enter valid full name!');
     }
-})
+});
 
 nodeEmail.addEventListener('change', () => {
-    let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let str = nodeEmail.value
+    const regex =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const str = nodeEmail.value;
     if (regex.test(str)) {
-        emailID = str
-        // validEmail = true
+        emailID = str;
     } else {
-        alert('enter valid email!')
-        // validEmail = false
+        alert('enter valid email!');
     }
-})
+});
 
 nodeMobileNo.addEventListener('change', () => {
-    let regex = /^([0-9]{10}$)/
-    let str = nodeMobileNo.value
+    const regex = /^([0-9]{10}$)/;
+    const str = nodeMobileNo.value;
     if (regex.test(str)) {
-        mobileNo = str
-        // validMobileNo = true
+        mobileNo = str;
     } else {
-        // validMobileNo = false
-        alert('enter valid mobile number')
+        alert('enter valid mobile number');
     }
-})
-
-
+});
 
 const clearRoles = () => {
     role.innerHTML = '<option>Select</option>';
@@ -62,213 +64,200 @@ const clearBranches = () => {
 };
 
 // OverallExperience and RelevantExperience
-let overallExpData, relevantExpData;                                 
+let overallExpData, relevantExpData;
 const loadOverallExpData = async () => {
-    const body = await getExperience('OVERALL')
-    overallExpData = body.data
+    const body = await getExperience('OVERALL');
+    overallExpData = body.data;
     // console.log(body.data)
-    setOverallEl()
-}
-loadOverallExpData()
+    setOverallEl();
+};
+loadOverallExpData();
 
 const loadRelevantExpData = async () => {
-    const body = await getExperience('RELEVANT')
-    relevantExpData = body.data
+    const body = await getExperience('RELEVANT');
+    relevantExpData = body.data;
     // console.log(body.data)
-    setReleventEl()
-}
-loadRelevantExpData()
+    setReleventEl();
+};
+loadRelevantExpData();
 
-function setOverallEl(){
-    for(let i=0; i<overallExpData.length; i++){
-        const OptEl = document.createElement('option')
-        OptEl.textContent = overallExpData[i].value
-        OptEl.setAttribute('value', overallExpData[i].id)
-        overallExperience.append(OptEl)
+function setOverallEl() {
+    for (let i = 0; i < overallExpData.length; i++) {
+        const OptEl = document.createElement('option');
+        OptEl.textContent = overallExpData[i].value;
+        OptEl.setAttribute('value', overallExpData[i].id);
+        overallExperience.append(OptEl);
     }
-    
 }
 
 function setReleventEl() {
     for (let i = 0; i < relevantExpData.length; i++) {
-        const OptEl = document.createElement('option')
-        OptEl.textContent = relevantExpData[i].value
-        OptEl.setAttribute('value', relevantExpData[i].id)
-        relaventExperience.append(OptEl)
+        const OptEl = document.createElement('option');
+        OptEl.textContent = relevantExpData[i].value;
+        OptEl.setAttribute('value', relevantExpData[i].id);
+        relaventExperience.append(OptEl);
     }
 }
 
 overallExperience.addEventListener('change', (e) => {
     experienceOverallID = e.target.value;
-})
+});
 
 relaventExperience.addEventListener('change', (e) => {
-    experienceRelavantID = e.target.value
-})
-
-
+    experienceRelavantID = e.target.value;
+});
 
 ////////////////// Departments and Roles ////////////////////////
 
-let departmentAndRoleData
+let departmentAndRoleData;
 const loadDepAndRoleData = async () => {
-    const body = await getDepAndRole()
-    departmentAndRoleData = body.data
+    const body = await getDepAndRole();
+    departmentAndRoleData = body.data;
     // console.log(body.data)
-    setDepEl()
-}
-loadDepAndRoleData()
+    setDepEl();
+};
+loadDepAndRoleData();
 
 function setDepEl() {
-    role.disabled = true
+    role.disabled = true;
     for (let i = 0; i < departmentAndRoleData.length; i++) {
-        const OptEl = document.createElement('option')
-        OptEl.textContent = departmentAndRoleData[i].name
-        OptEl.setAttribute('value', departmentAndRoleData[i].id)
-        department.append(OptEl)
+        const OptEl = document.createElement('option');
+        OptEl.textContent = departmentAndRoleData[i].name;
+        OptEl.setAttribute('value', departmentAndRoleData[i].id);
+        department.append(OptEl);
     }
 }
 
 department.addEventListener('change', (e) => {
-    clearRoles()
-    const id = e.target.value
-    setRoleEl(id)
-})
+    clearRoles();
+    const id = e.target.value;
+    setRoleEl(id);
+});
 
 function setRoleEl(selectedDep) {
-    let rolesData
+    let rolesData;
     for (let i = 0; i < departmentAndRoleData.length; i++) {
-        if(selectedDep === departmentAndRoleData[i].id){
-            rolesData = departmentAndRoleData[i].roles
+        if (selectedDep === departmentAndRoleData[i].id) {
+            rolesData = departmentAndRoleData[i].roles;
         }
     }
-    for(let i=0; i<rolesData.length; i++){
-        const OptEl = document.createElement('option')
-        OptEl.textContent = rolesData[i].name
-        OptEl.setAttribute('value', rolesData[i].id)
-        role.append(OptEl)
+    for (let i = 0; i < rolesData.length; i++) {
+        const OptEl = document.createElement('option');
+        OptEl.textContent = rolesData[i].name;
+        OptEl.setAttribute('value', rolesData[i].id);
+        role.append(OptEl);
     }
-    role.disabled = false
+    role.disabled = false;
     role.addEventListener('change', (e) => {
-        roleID = e.target.value
-    })
+        roleID = e.target.value;
+    });
 }
 
 ////////////// zones and branches ///////////////////
 
 let zoneData;
 const loadZonesData = async () => {
-    const body = await getZones()
-    zoneData = body.data
-    setZonesEl()
-}
-loadZonesData()
+    const body = await getZones();
+    zoneData = body.data;
+    setZonesEl();
+};
+loadZonesData();
 
-function setZonesEl(){
-    branch.disabled = true
-    for(let i=0; i<zoneData.length; i++){
-        const OptEl = document.createElement('option')
-        OptEl.textContent = zoneData[i]
-        OptEl.setAttribute('value', zoneData[i])
-        zone.append(OptEl)
+function setZonesEl() {
+    branch.disabled = true;
+    for (let i = 0; i < zoneData.length; i++) {
+        const OptEl = document.createElement('option');
+        OptEl.textContent = zoneData[i];
+        OptEl.setAttribute('value', zoneData[i]);
+        zone.append(OptEl);
     }
 }
 
 let BranchesData;
 const loadBranchesData = async (selectedZone) => {
-    const body = await getBranches(selectedZone)
-    BranchesData = body.data
-    setBranchesEl()
-}
+    const body = await getBranches(selectedZone);
+    BranchesData = body.data;
+    setBranchesEl();
+};
 
-function setBranchesEl(){
-    for(let i=0; i<BranchesData.length; i++){
-        const OptEl = document.createElement('option')
-        OptEl.textContent = BranchesData[i].branch
-        OptEl.setAttribute('value', BranchesData[i].id)
-        branch.append(OptEl)
+function setBranchesEl() {
+    for (let i = 0; i < BranchesData.length; i++) {
+        const OptEl = document.createElement('option');
+        OptEl.textContent = BranchesData[i].branch;
+        OptEl.setAttribute('value', BranchesData[i].id);
+        branch.append(OptEl);
     }
-    branch.disabled = false
+    branch.disabled = false;
     branch.addEventListener('change', (e) => {
-        jobLocationID = e.target.value
-    })
-}
-
-zone.addEventListener('change', (e) => {
-    clearBranches()
-    let selectedZone = e.target.value
-    loadBranchesData(selectedZone)
-})
-
-
-
-
-
-const resume = document.getElementById("attachFile")
-const upload = (e) => {
-    // e.preventDefault()
-    const form = new FormData();
-    form.append("file", resume.files[0]);
-
-    var settings = {
-        "url": "https://api-hfc.techchefz.com/icicihfc-micro-service/document/reference/upload/v2",
-        "method": "POST",
-        "timeout": 0, 
-        "processData": false,
-        "mimeType": "multipart/form-data",
-        "contentType": false,
-        "data": form
-    };
-
-    $.ajax(settings).done(function (response) {
-        const data = JSON.parse(response).data
-               resumeID = data.id
-            resumeName = data.name
-            resumeNonce = data.nonce
-
-       
+        jobLocationID = e.target.value;
     });
 }
 
+zone.addEventListener('change', (e) => {
+    clearBranches();
+    let selectedZone = e.target.value;
+    loadBranchesData(selectedZone);
+});
 
-function formSubmit (){
+const resume = document.getElementById('attachFile');
+const upload = (e) => {
+    // e.preventDefault()
+    const form = new FormData();
+    form.append('file', resume.files[0]);
+
+    var settings = {
+        url: 'https://api-hfc.techchefz.com/icicihfc-micro-service/document/reference/upload/v2',
+        method: 'POST',
+        timeout: 0,
+        processData: false,
+        mimeType: 'multipart/form-data',
+        contentType: false,
+        data: form,
+    };
+
+    $.ajax(settings).done(function (response) {
+        const data = JSON.parse(response).data;
+        resumeID = data.id;
+        resumeName = data.name;
+        resumeNonce = data.nonce;
+    });
+};
+
+function formSubmit() {
     const obj = {
         fullName: fullName,
         emailId: emailID,
         mobileNumber: mobileNo,
         experienceOverallId: experienceOverallID,
-        experienceRelaventId: experienceRelavantID,
+        experienceRelevantId: experienceRelavantID,
         roleId: roleID,
         jobLocationId: jobLocationID,
         resumeDocRefId: resumeID,
         resumeDocRefFileName: resumeName,
         resumeDocRefNonce: resumeNonce,
-        adLoginNonce: ""
-    }
+        adLoginNonce: '',
+    };
     const settings = {
-        "url": "https://api-hfc.techchefz.com/icicihfc-micro-service/rms/candidate/submit/form",
-        "method": "POST",
-        "timeout": 0,
-        "headers": {
-            "Content-Type": "application/json",
+        url: 'https://api-hfc.techchefz.com/icicihfc-micro-service/rms/candidate/submit/form',
+        method: 'POST',
+        timeout: 0,
+        headers: {
+            'Content-Type': 'application/json',
         },
-        "data": JSON.stringify(obj),
+        data: JSON.stringify(obj),
     };
 
     $.ajax(settings).done(function (response) {
         console.log(response);
     });
-    console.log(obj)
+    console.log(obj);
 }
 
-
-
 button.addEventListener('click', () => {
-    formSubmit()
-})
+    formSubmit();
+});
 
 
-
-// the coding train
+// // the coding train
 
 
